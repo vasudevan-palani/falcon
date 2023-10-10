78d42e11d44a40c4c579dcf69f712d6ea35188d3
Vasudevan PALANI <vasudevan.palani@gmail.com>
2023-10-09 20:19:50 -0400

    refactored list items

---
+++ src/components/ListFiles.vue
@@ -0,0 +1,106 @@
+<script setup lang="ts">
+
+// Imports
+//
+import { reactive, ref, watchEffect, onMounted } from "vue";
+import { SearchIcon } from "@vue-icons/feather";
+import { ElTreeV2 } from 'element-plus'
+import type { TreeNode } from 'element-plus/es/components/tree-v2/src/types'
+//Properties
+const props = defineProps<{ items:any }>();
+const emit = defineEmits(["onFileSelect"]);
+
+//Interfaces
+//
+interface Tree {
+  id: string
+  label: string
+  children?: Tree[]
+}
+
+// Variables
+//
+const filter = ref("");
+
+//Global variables
+//
+const treeprops = {
+  value: 'id',
+  label: 'label',
+  children: 'children',
+}
+
+//Event Handlers
+//
+const itemSelected = (item: any,node:any,event:any) => {
+    console.log(item)
+
+    if(item.value?.children == undefined){
+      emit("onFileSelect",item);
+    }
+    // if(item.isLeaf == false){
+    //   workspaceDirectory.value = workspaceDirectory.value + "/" + item.Name
+    //   return
+    // }
+    // emit("onRequestClicked",item);
+};
+const treeRef = ref<InstanceType<typeof ElTreeV2>>()
+const onQueryChanged = (query: string) => {
+  // TODO: fix typing when refactor tree-v2
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  treeRef.value!.filter(query)
+}
+
+const filterMethod = (query: string, node: TreeNode) => {
+  return node.label!.includes(query)
+}
+
+
+// Click handlers
+//
+
+
+
+//Utility function
+//
+
+
+watchEffect(()=>{
+
+})
+</script>
+
+<template>
+      
+  <el-row class="searchfilter">
+    <el-input
+      v-model="filter"
+      @input = "onQueryChanged"
+      class="w-50 m-2"
+      size="large"
+      placeholder="Search"
+      :suffix-icon="SearchIcon"
+    />
+  </el-row>
+    <el-tree-v2 class="collection-list"
+    ref="treeRef"
+    :filter-method="filterMethod"
+    @node-click = "itemSelected"
+    :data="items"
+    :props = "treeprops"
+    :height="208"
+  />
+</template>
+
+<style scoped>
+.collection-list {
+  text-align: left;
+  border: #EEE 1px solid;
+  border-radius: 5px;
+  margin: 10px;
+  height:815px;
+}
+.searchfilter {
+  margin: 10px;
+}
+</style>
--- src/components/WorkspaceFolder.vue
+++ src/components/WorkspaceFolder.vue
@@ -3,22 +3,17 @@
 //
 import { reactive, ref, watchEffect, onMounted } from "vue";
 import { SearchIcon, FolderIcon } from "@vue-icons/feather";
-import ListItems from "./ListItems.vue";
+import ListFiles from "./ListFiles.vue";
 import { ipcRenderer } from "electron";
 import {truncateText} from "../services/utils";
 const path = require('path');
 const fs = require('fs');
-import { ElTreeV2 } from 'element-plus'
-import type { TreeNode } from 'element-plus/es/components/tree-v2/src/types'
+
 
 
 //interfaces
 //
-interface Tree {
-  id: string
-  label: string
-  children?: Tree[]
-}
+
 
 //Properties
 const props = defineProps<{msg:boolean}>();
@@ -26,42 +21,19 @@ const emit = defineEmits(["onRequestClicked"]);
 
 // Variables
 //
-const filter = ref("");
+
 const error = ref("");
 const items = ref<any[]>([]);
 const workspaceDirectory = ref("")
 
 //Global variables
 //
-const treeprops = {
-  value: 'id',
-  label: 'label',
-  children: 'children',
-}
+
 
 //Event Handlers
 //
-const itemSelected = (item: any,node:any,event:any) => {
-    console.log(item)
-
-    if(item.value?.children == undefined){
-      emit("onRequestClicked",item);
-    }
-    // if(item.isLeaf == false){
-    //   workspaceDirectory.value = workspaceDirectory.value + "/" + item.Name
-    //   return
-    // }
-    // emit("onRequestClicked",item);
-};
-const treeRef = ref<InstanceType<typeof ElTreeV2>>()
-const onQueryChanged = (query: string) => {
-  // TODO: fix typing when refactor tree-v2
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  treeRef.value!.filter(query)
-}
-
-const filterMethod = (query: string, node: TreeNode) => {
-  return node.label!.includes(query)
+const onFileSelect = (item:any)=>{
+  emit('onRequestClicked',item)
 }
 
 // Click handlers
@@ -127,48 +99,17 @@ watchEffect(() => {
     <el-button @click="chooseWorkspace">Choose</el-button>
   </el-col>
   </el-row>
-  
-  <el-row class="searchfilter" v-if="workspaceDirectory != ''">
-    <el-input
-      v-model="filter"
-      @input = "onQueryChanged"
-      class="w-50 m-2"
-      size="large"
-      placeholder="Search"
-      :suffix-icon="SearchIcon"
-    />
-  </el-row>
-  <el-row v-if="workspaceDirectory != ''">
+  <el-row v-if="workspaceDirectory != ''" class="workspace-folder-row">
     <el-col>
       <el-text :title="workspaceDirectory">{{ truncateText(workspaceDirectory,38) }} &nbsp;&nbsp;</el-text>
       <el-button :icon="FolderIcon" @click="chooseWorkspace"></el-button>
     </el-col>
   </el-row>
-  <!--ListItems class="collection-list" v-if="workspaceDirectory != ''"
-    :has-more="false"
-    :items="items"
-    @item-selected="itemSelected"
-    :filter="filter"
-  /-->
-  <el-tree-v2 class="collection-list"
-    ref="treeRef"
-    :filter-method="filterMethod"
-    @node-click = "itemSelected"
-    :data="items"
-    :props = "treeprops"
-    :height="208"
-  />
+  <ListFiles :items="items" @on-file-select="onFileSelect"></ListFiles>
 </template>
 
 <style scoped>
-.collection-list {
-  text-align: left;
-  border: #EEE 1px solid;
-  border-radius: 5px;
-  margin: 10px;
-  height:815px;
-}
-.searchfilter {
-  margin: 10px;
+.workspace-folder-row{
+  margin-top:10px;
 }
 </style>
